350cabdf35e5f0566957acb6eaf9898859dcb622
Mustafa ÇÖREKCİ <corekci_mustafa@hotmail.com>
2019-03-30 15:06:58 +0300

    Sensor

---
+++ Sensor.py
@@ -0,0 +1,41 @@
+
+class Sensor:
+    def __init__(self, name, id, kind, value):
+        self.name=name
+        self.id=id
+        self.kind=kind
+        self.value=value
+        # self.last100values=Queue.Queue()
+        self.last100values=list()
+        pass
+
+    def getName(self):
+        return self.name
+    def setName(self,name):
+        self.name=name
+
+    def getId(self):
+        return self.id
+    def setId(self,id):
+        self.id=id
+
+    def getKind(self):
+        return self.kind
+    def setKind(self,kind):
+        self.kind=kind
+
+    def getValue(self):
+        return self.value
+    def setValue(self,value):
+        self.value=value
+        self.last100values.insert(0,value)
+        if len(self.last100values)>=100:
+            self.last100values.remove(len(self.last100values)-1)
+        # self.last100values.put(value)
+        # if len(self.last100values)>=100:
+        #     self.last100values.get()
+
+    def getValue(self):
+        return self.value
+    def setValue(self,value):
+        self.value=value
